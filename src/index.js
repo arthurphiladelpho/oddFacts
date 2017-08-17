15dc2697c49bdaa35bc17dc37ab273ff68dc2c1f
Arthur Philadelpho <arthur22rj@gmail.com>
2017-08-17 17:39:28 -0300

    Skill has been submitted for certification.

--- src/index.js
+++ src/index.js
@@ -9,9 +9,9 @@ var Alexa = require('alexa-sdk');
 //Make sure to enclose your value in quotes, like this: var APP_ID = "amzn1.ask.skill.bb4045e6-b3e8-4133-b650-72923c5980f1";
 var APP_ID = undefined;
 
-var SKILL_NAME = "Space Facts";
+var SKILL_NAME = "Odd Facts";
 var GET_FACT_MESSAGE = "Here's your fact: ";
-var HELP_MESSAGE = "You can say tell me a space fact, or, you can say exit... What can I help you with?";
+var HELP_MESSAGE = "You can say tell me an odd fact, or, you can say exit... What can I help you with?";
 var HELP_REPROMPT = "What can I help you with?";
 var STOP_MESSAGE = "Goodbye!";
 
@@ -19,19 +19,46 @@ var STOP_MESSAGE = "Goodbye!";
 //TODO: Replace this data with your own.  You can find translations of this data at http://github.com/alexa/skill-sample-node-js-fact/data
 //=========================================================================================================================================
 var data = [
-    "A year on Mercury is just 88 days long.",
-    "Despite being farther from the Sun, Venus experiences higher temperatures than Mercury.",
-    "Venus rotates counter-clockwise, possibly because of a collision in the past with an asteroid.",
-    "On Mars, the Sun appears about half the size as it does on Earth.",
-    "Earth is the only planet not named after a god.",
-    "Jupiter has the shortest day of all the planets.",
-    "The Milky Way galaxy will collide with the Andromeda Galaxy in about 5 billion years.",
-    "The Sun contains 99.86% of the mass in the Solar System.",
+    "It is physically impossible for you to lick your elbow.",
+    "Like fingerprints, everyone's tongue print is different.",
+    "Your heart beats over one hundred thousand times a day.",
+    "It takes approximately twelve hours for food to entirely digest.",
+    "Ostriches can run faster than horses, and the male ostriches can roar like lions.",
+    "Bats are the only mammals that can fly.",
+    "Kangaroos use their tails for balance, so if you lift a kangaroo’s tail off the ground, it can’t hop.",
     "The Sun is an almost perfect sphere.",
-    "A total solar eclipse can happen once every 1 to 2 years. This makes them a rare event.",
+    "A total solar eclipse can happen once every one to two years. This makes them a rare event.",
     "Saturn radiates two and a half times more energy into space than it receives from the sun.",
-    "The temperature inside the Sun can reach 15 million degrees Celsius.",
-    "The Moon is moving approximately 3.8 cm away from our planet every year."
+    "The temperature inside the Sun can reach fifteen million degrees Celsius.",
+    "Tigers not only have stripes on their fur, they also have them on their skin.",
+    "No two tigers ever have the same stripes.",
+    "A cat has thirty two muscles in each ear.",
+    "Elephants can smell water up to three miles away.",
+    "There are only three mammals that undergo menopause: elephants, humpback whales and humans.",
+    "The Arctic Circle is the most northerly of the abstract five major circles of latitude as shown on maps of the Earth.",
+    "Adaptations are the most important features of all organisms.",
+    "There is a narrow passage of water between Russia and the United States, it is called the Bering Strait.",
+    "The micro biome is the genetic material of all the microbes that live on and inside the human body.",
+    "Hinduism is the oldest religion in the world.",
+    "Yoga is a group of physical, mental, and spiritual practices or disciplines which originated in ancient India.",
+    "Yoga can reduce insomnia.",
+    "Eagles have excellent eyesight.",
+    "Dogs and cats both slurp water the same way.",
+    "Dogs only have sweat glands in their paws.",
+    "Unlike other cats, lions are very social animals.",
+    "Lions live in groups, called pride.",
+    "Most sharks species will drown if they stop moving.",
+    "The polar bear is a carnivorous bear whose native range lies largely within the Arctic Circle, encompassing the Arctic Ocean, its surrounding seas and surrounding land masses.",
+    "The giant panda, also known as panda bear or simply panda, is a bear native to south central China.",
+    "The red panda, also called the lesser panda, the red bear-cat, and the red cat-bear, is a mammal native to the eastern Himalayas and southwestern China.",
+    "The snow leopard or ounce is a large cat native to the mountain ranges of Central and South Asia.",
+    "The jaguar is a big cat, the only one of his species native to the Americas.",
+    "The cougar, also commonly known as the mountain lion, puma, panther, is native to the Americas.",
+    "The Bengal tiger is the most numerous of the tiger subspecies.",
+    "The bald eagle is a bird of prey found in North America.",
+    "Harpy eagles eat monkeys sometimes.",
+    "Chalicotheres is an extinct group of herbivorous, odd-toed ungulate mammals spread throughout North America",
+    "The giraffe is a genus of African even-toed ungulate mammals, the tallest living terrestrial animals and the largest ruminants."
 ];
 
 //=========================================================================================================================================
